fe20a7ecb52338a45cc826221b254665b5061b30
Carlos Mora <charlymora332@gmail.com>
2024-10-03 11:37:47 -0500

    login and register x2

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,8 +9,45 @@ import ojoPass from "./assets/login/ojoPass.png";
 
 export default function Register() {
   return (
-    <div>
-      
+    <div className="body">
+    <div className="contenedorForm">
+      <h1>Registrese</h1>
+      <div className="contenedortext">
+        <form className="">
+          <Input label={""} placeholder={"Nombre:"} type={"text"} />
+          <Input label={""} placeholder={"Teléfono:"} type={"number"} />
+          <Input label={""} placeholder={"E-mail:"} type={"email"} />
+          <div className="inputPass">
+            <Input label={""} placeholder={"Contraseña:"} type={"password"} />
+
+            <img src={ojoPass} alt="" />
+          </div>
+
+          <div className="inputPass">
+            <Input label={""} placeholder={"Repita la Contraseña:"} type={"password"} />
+
+            <img src={ojoPass} alt="" />
+          </div>
+
+          <br />
+          <Boton texto={"Unirse"} />
+          <h6 className="decoracion">o</h6>
+        </form>
+
+        <button className="loginGoogle">
+          <img src={logoGoogle} alt="Logo" />
+          <h6>Ingresar con Google</h6>
+        </button>
+
+        <h6>
+        ¿Ya tiene una cuenta? 
+          <a href="https://www.youtube.com/watch?v=k3Bf5VexjIo&list=RDfVKX20klaUU&index=13">
+            Iniciar sesión
+          </a>
+        </h6>
+      </div>
     </div>
+    <img src={imgPrincipal} alt="img de moto" />
+  </div>
   )
 }
