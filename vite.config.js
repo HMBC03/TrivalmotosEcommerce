68ee9267c8d820613c4e91de43909d16363ab574
Carlos Mora <charlymora332@gmail.com>
2024-10-07 16:15:46 -0500

    archivos base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],  
-  base: "https://charlymora332.github.io/121212",
+  
 
 })
