278ba1867298c42863579f00cb25e8459555b96c
Carlos Mora <charlymora332@gmail.com>
2024-10-17 13:09:32 -0500

    archivos base x2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,72 @@
-import React from "react";
-
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
+
+// Importación de iconos
 import iconoCarrito from "../assets/home/iconoCarrito.svg";
 import iconoFavoritos from "../assets/home/iconoFavoritos.svg";
 import iconoLupa from "../assets/home/iconoLupa.svg";
 import iconoUsuario from "../assets/home/iconoUsuario.svg";
-import { Link } from "react-router-dom";
+import iconoMenuAbi from "../assets/home/iconoMenuAbi.svg";
+import iconoMenuCer from "../assets/home/iconoMenuCer.svg";
 
 export default function Header() {
+  const headerRef = useRef(null); // Referencia al header
+  const [menuOpen, setMenuOpen] = useState(false); // Estado del menú
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+    headerRef.current.style.display = menuOpen ? "none" : "flex"; // Alterna entre mostrar y ocultar
+  };
+
+  // Función para ocultar el menú al hacer clic en cualquier enlace
+  const handleLinkClick = () => {
+    setMenuOpen(false);
+    headerRef.current.style.display = "none"; // Asegura que el menú esté oculto
+  };
+
   return (
-    <header>
-      <Link to="/Trivalmotos" ><h3>Trivalmotos</h3></Link>
-
-      <nav className="contenedorRedireccion">
-        <ul>
-          <li >
-            <Link to="/Trivalmotos/tienda" >Tienda</Link>
-          </li>
-          <li >
-          <Link to="/Trivalmotos/contacto">Contacto</Link>
-          </li>
-          <li>
-            <Link to="/Trivalmotos/EnDesarrollo" >Ofertas</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <div className="contenedorIconos">
-        <Link to="/Trivalmotos/EnDesarrollo" aria-label="Buscar" className="linkPag">
-          <img src={iconoLupa} alt="Buscar" />
-        </Link>
-     
-        <Link to="/Trivalmotos/login" aria-label="Perfil" className="linkPag">
-         
-          <img src={iconoUsuario} alt="Usuario" />
-         
-        </Link>
-
-        <Link to="/Trivalmotos/EnDesarrollo" aria-label="Favoritos" className="linkPag" >
-          <img src={iconoFavoritos} alt="Favoritos" />
-        </Link>
-        <Link to="/Trivalmotos/EnDesarrollo" aria-label="Carrito de compras" className="linkPag">
-          <img src={iconoCarrito} alt="Carrito" />
-        </Link>
+    <div className="ctnHeader">
+      <div className="menuDesplegable" onClick={toggleMenu}>
+        <img alt="" src={menuOpen ? iconoMenuCer : iconoMenuAbi} />
       </div>
-    </header>
+
+      <header>
+        <Link to="/Trivalmotos">
+          <h3>Trivalmotos</h3>
+        </Link>
+
+        <div ref={headerRef} className="ctnNavegacion">
+          <nav className="contenedorRedireccion">
+            <ul>
+              <li>
+                <Link to="/Trivalmotos/tienda" onClick={handleLinkClick}>Tienda</Link>
+              </li>
+              <li>
+                <Link to="/Trivalmotos/contacto" onClick={handleLinkClick}>Contacto</Link>
+              </li>
+              <li>
+                <Link to="/Trivalmotos/EnDesarrollo" onClick={handleLinkClick}>Ofertas</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="contenedorIconos">
+            <Link to="/Trivalmotos/EnDesarrollo" aria-label="Buscar" className="linkPag" onClick={handleLinkClick}>
+              <img src={iconoLupa} alt="Buscar" />
+            </Link>
+            <Link to="/Trivalmotos/login" aria-label="Perfil" className="linkPag" onClick={handleLinkClick}>
+              <img src={iconoUsuario} alt="Usuario" />
+            </Link>
+            <Link to="/Trivalmotos/EnDesarrollo" aria-label="Favoritos" className="linkPag" onClick={handleLinkClick}>
+              <img src={iconoFavoritos} alt="Favoritos" />
+            </Link>
+            <Link to="/Trivalmotos/EnDesarrollo" aria-label="Carrito de compras" className="linkPag" onClick={handleLinkClick}>
+              <img src={iconoCarrito} alt="Carrito" />
+            </Link>
+          </div>
+        </div>
+      </header>
+    </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,55 +1,93 @@
-
 import "./Login.css";
 import Input from "../components/Input";
 import Boton from "../components/Boton";
 import imgPrincipal from "../assets/login/principal.jpg";
 import ojoPass from "../assets/login/ojoPass.png";
-import logoGoogle from '../assets/login/logoGoogle.svg'
+import logoGoogle from "../assets/login/logoGoogle.svg";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import React, { useState } from "react";
+import axios  from 'axios'
+
 
 function Login() {
+  const [credenciales, setCredenciales] = useState({ user: "", pass: "" });
 
-  return (
+
+  const actualizarInput = ({ target }) => {
+    const { name, value } = target;
+    setCredenciales({
+      ...credenciales,
+      [name]: value,
+    });
+
+
+  };
+
+  const enviarDatos = (event) =>{
+    event.preventDefault(); 
+  console.log(credenciales)
+
+  axios.post('http://localhost:4000/api/login', credenciales)
+
+  .then(({data})=>{
+    console.log(data)
+  })
+
+  .catch(({response})=>{
+    console.log(response)
+  })
+  }
+
+
+    return (
     <div className="body">
       <div className="contenedorForm">
-        <Header/>
-      <h1>Iniciar Sesión</h1> 
-        <div className="contenedortext"> 
-
-  
-     
-        
+        <Header />
+        <h1>Iniciar Sesión</h1>
+        <div className="contenedortext">
           <form className="">
-        
-          <Input label={""} placeholder={"E-mail:"} type={"email"} />
-          <div className="inputPass">
-                 
-            <Input label={""} placeholder={"Contraseña:"} type={"password"} /> 
-            
-            <img src={ojoPass} alt="" />
+            <Input
+              label={""}
+              placeholder={"E-mail:"}
+              type={"email"}
+              value={credenciales.user}
+              funcion={actualizarInput}
+              name={"user"}
+            />
+            <div className="inputPass">
+              <Input
+                label={""}
+                placeholder={"Contraseña:"}
+                type={"password"}
+                value={credenciales.pass}
+                funcion={actualizarInput}
+                name={"pass"}
+              />
+
+              <img src={ojoPass} alt="" />
             </div>
 
-          <br/>
-          <Boton texto={"Ingresar"} />
-  <h6 className="decoracion">o</h6>
-
-  </form>
-  {/* <div >
+            <br />
+            <Boton texto={"Ingresar"} funcion={enviarDatos} />
+            <h6 className="decoracion">o</h6>
+          </form>
+          {/* <div >
     <img src={logoGoogle} alt="logo de Google" />
     <Boton texto={"⌘  Ingresar con Google "} bgc="white" style={{ color: "black"}} />
   </div> */}
-  <button className="loginGoogle">
-      <img src={logoGoogle} alt="Logo"/>
-      <h6>Ingresar con Google</h6>
-    </button>
+          <button className="loginGoogle">
+            <img src={logoGoogle} alt="Logo" />
+            <h6>Ingresar con Google</h6>
+          </button>
 
-  <h6>¿No tiene una cuenta?<Link to="/Trivalmotos/register">Registrese</Link></h6>
-      
-      </div>
+          <h6>
+            ¿No tiene una cuenta?
+            <Link to="/Trivalmotos/register">Registrese</Link>
+          </h6>
+        </div>
       </div>
       <img src={imgPrincipal} alt="img de moto" />
-
     </div>
   );
 }
