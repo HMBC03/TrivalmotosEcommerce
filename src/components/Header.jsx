0d8275e976a0ba616bc12cc25ed30d7f858d5a0a
Carlos Mora <charlymora332@gmail.com>
2024-10-21 14:05:46 -0500

    correccion de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
@@ -10,61 +10,112 @@ import iconoUsuario from "../assets/home/iconoUsuario.svg";
 import iconoMenuAbi from "../assets/home/iconoMenuAbi.svg";
 import iconoMenuCer from "../assets/home/iconoMenuCer.svg";
 
-export default function Header() {
-  const headerRef = useRef(null); // Referencia al header
+export default function Header({estilosTexto, estilosIcono}) {
+  const header = useRef(); // Referencia al header
+  const headerRef = useRef(); // Referencia al header
+  const headerRefIcono = useRef(); // Referencia al header
+
   const [menuOpen, setMenuOpen] = useState(false); // Estado del menú
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
-    headerRef.current.style.display = menuOpen ? "none" : "flex"; // Alterna entre mostrar y ocultar
+
+    if (menuOpen) {
+      headerRef.current.classList.add("linkOcultar");
+      headerRefIcono.current.classList.add("linkOcultar");
+    } else {
+      headerRef.current.classList.remove("linkOcultar");
+      headerRefIcono.current.classList.remove("linkOcultar");
+    }
   };
+  //agg clase para que funcione el disenio responseve en telefonos
+  //desplega u oculta el header 
+  useEffect(() => {
+    if (header.current) {
+      if (menuOpen) {
+        header.current.classList.add("desplegado");
+      } else {
+        header.current.classList.remove("desplegado");
+      }
+    }
+  }, [menuOpen]);
 
   // Función para ocultar el menú al hacer clic en cualquier enlace
   const handleLinkClick = () => {
     setMenuOpen(false);
-    headerRef.current.style.display = "none"; // Asegura que el menú esté oculto
+    if (menuOpen) {
+      headerRef.current.classList.add("linkOcultar");
+      headerRefIcono.current.classList.add("linkOcultar");
+    } else {
+      headerRef.current.classList.remove("linkOcultar");
+      headerRefIcono.current.classList.remove("linkOcultar");
+    }
   };
 
   return (
     <div className="ctnHeader">
       <div className="menuDesplegable" onClick={toggleMenu}>
-        <img alt="" src={menuOpen ? iconoMenuCer : iconoMenuAbi} />
+        <img alt="" src={menuOpen ? iconoMenuAbi : iconoMenuCer} />
       </div>
 
-      <header>
+      <header ref={header}>
         <Link to="/Trivalmotos">
           <h3>Trivalmotos</h3>
         </Link>
 
-        <div ref={headerRef} className="ctnNavegacion">
-          <nav className="contenedorRedireccion">
-            <ul>
-              <li>
-                <Link to="/Trivalmotos/tienda" onClick={handleLinkClick}>Tienda</Link>
-              </li>
-              <li>
-                <Link to="/Trivalmotos/contacto" onClick={handleLinkClick}>Contacto</Link>
-              </li>
-              <li>
-                <Link to="/Trivalmotos/EnDesarrollo" onClick={handleLinkClick}>Ofertas</Link>
-              </li>
-            </ul>
-          </nav>
+        <nav className="contenedorRedireccion linkOcultar" ref={headerRef} style={estilosTexto}>
+          <ul>
+            <li>
+              <Link to="/Trivalmotos/tienda" onClick={handleLinkClick}>
+                Tienda
+              </Link>
+            </li>
+            <li>
+              <Link to="/Trivalmotos/contacto" onClick={handleLinkClick}>
+                Contacto
+              </Link>
+            </li>
+            <li>
+              <Link to="/Trivalmotos/EnDesarrollo" onClick={handleLinkClick}>
+                Ofertas
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-          <div className="contenedorIconos">
-            <Link to="/Trivalmotos/EnDesarrollo" aria-label="Buscar" className="linkPag" onClick={handleLinkClick}>
-              <img src={iconoLupa} alt="Buscar" />
-            </Link>
-            <Link to="/Trivalmotos/login" aria-label="Perfil" className="linkPag" onClick={handleLinkClick}>
-              <img src={iconoUsuario} alt="Usuario" />
-            </Link>
-            <Link to="/Trivalmotos/EnDesarrollo" aria-label="Favoritos" className="linkPag" onClick={handleLinkClick}>
-              <img src={iconoFavoritos} alt="Favoritos" />
-            </Link>
-            <Link to="/Trivalmotos/EnDesarrollo" aria-label="Carrito de compras" className="linkPag" onClick={handleLinkClick}>
-              <img src={iconoCarrito} alt="Carrito" />
-            </Link>
-          </div>
+        <div className="contenedorIconos linkOcultar" ref={headerRefIcono} style={estilosIcono}>
+          <Link
+            to="/Trivalmotos/EnDesarrollo"
+            aria-label="Buscar"
+            className="linkPag"
+            onClick={handleLinkClick}
+          >
+            <img src={iconoLupa} alt="Buscar" />
+          </Link>
+          <Link
+            to="/Trivalmotos/login"
+            aria-label="Perfil"
+            className="linkPag"
+            onClick={handleLinkClick}
+          >
+            <img src={iconoUsuario} alt="Usuario" />
+          </Link>
+          <Link
+            to="/Trivalmotos/EnDesarrollo"
+            aria-label="Favoritos"
+            className="linkPag"
+            onClick={handleLinkClick}
+          >
+            <img src={iconoFavoritos} alt="Favoritos" />
+          </Link>
+          <Link
+            to="/Trivalmotos/EnDesarrollo"
+            aria-label="Carrito de compras"
+            className="linkPag"
+            onClick={handleLinkClick}
+          >
+            <img src={iconoCarrito} alt="Carrito" />
+          </Link>
         </div>
       </header>
     </div>
